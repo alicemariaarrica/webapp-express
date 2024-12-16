441095f67f969893a48570f2a395e11723fa5406
alicemariaarrica <alicemariaarrica@gmail.com>
2024-12-16 20:32:47 +0100

    comments-commit

--- database/connection.js
+++ database/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql2')
+
+//adesso creo un oggetto per impostare i dati della configurazione per effettuare la connessione al database
+// effettivamente la connessione avverà dopo una query
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'backend'
+    host: 'localhost', //argomento
+    port: 3306, //argomento
+    user: 'root', //argomento
+    password: 'password', //argomento
+    database: 'backend' //argomento
 
 })
 
+//connect è un metodo dell'oggetto connection (creato poc'anzi attraverso la funzione mysql.createConnection ) 
+// stabilisce quasi con forza la connessione al database
 connection.connect((err) => {
     if (err) {
         console.log('Error connecting to database', err);
