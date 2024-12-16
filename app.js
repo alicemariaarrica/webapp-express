12581f1c9fe27a2d8e99a4dfbacd385898e91dce
alicemariaarrica <alicemariaarrica@gmail.com>
2024-12-16 15:51:02 +0100

    db-commit

--- app.js
+++ app.js
@@ -4,32 +4,3 @@ const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
-
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-});
-
-
-pool.getConnection((err, connection) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-    } else {
-        console.log('Connected to the database!');
-        connection.release();
-    }
-});
-
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the Repo Express Blog!');
-});
-
-
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
